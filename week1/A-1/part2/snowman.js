6386b2cc5bbed40ecbfcc3da6c3af05b00948885
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-01-24 17:38:29 -0600

    created new snowcreature and added interactions

--- week1/A-1/part2/snowman.js
+++ week1/A-1/part2/snowman.js
@@ -1,40 +1,37 @@
 let grid = undefined;
 let stroke1 = prompt("enter a basic color name in lower case", "gray");
 let stroke2 = prompt("enter another basic color name in lower case", "purple");
+let stroke3 = prompt("enter another baisc color name in lower case", "orange")
+let stroke4 = prompt("enter another baisc color name in lower case", "black")
 function setup() {
-    createCanvas(1000, 800);
+    createCanvas(1000, 1000);
     background("#ccc");
-    grid = loadImage("../images/100px_grid.png");
 }
 
 function draw() {
-    background(grid);
-    //snowman legs
-    fill("#f1f1f1");
-    strokeWeight(20);
-    stroke(stroke1);
-    //left leg
-    ellipse(350, 650, 200);
-    //right leg
-    ellipse(650, 650, 200);
     //body
-    ellipse(500, 450, 350, 400);
+    stroke(stroke1);
+    ellipse(500, 700, 450);
+    ellipse(500, 450, 350);
     //head
-    ellipse(500, 200, 200);
+    ellipse(500, 225, 200);
     //hat brim
     stroke(stroke2);
-    strokeWeight(40);
+    strokeWeight(35);
     line(400, 120, 600, 120);
     //hat body
     quad(400, 50, 600, 50, 550, 120, 450, 120);
     //eyes
     stroke(0);
-    strokeWeight(50);
-    point(425, 200);
-    point(575, 200);
-    //mouth
-    noFill();
-    strokeWeight(10);
-    arc(500, 200, 80, 80, 0, HALF_PI);
-
+    strokeWeight(30);
+    point(450, 200);
+    point(550, 200);
+    //nose
+    stroke(stroke3)
+    ellipse(500, 225, 15, 5);
+    //buttons
+    stroke(stroke4)
+    ellipse(500, 375, 15, 5);
+    ellipse(500, 450, 15, 5);
+    ellipse(500, 525, 15, 5);
 }
