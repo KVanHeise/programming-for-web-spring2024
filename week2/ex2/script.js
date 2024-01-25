13c5aba2bf052fb537a97d2a16d3b400786e1279
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-01-24 23:06:21 -0600

    started filling array objects

--- week2/ex2/script.js
+++ week2/ex2/script.js
@@ -6,8 +6,35 @@ const myFavoriteGames = [
         title: "Dead by Daylight",
         type: "video game",
         genre: "horror",
-        numberOfPlayers: "up to 5",
+        numberOfPlayers: "1 to 5",
         shortDescription: "co-op horror survival game"
     },
-
+    {
+        title: "Baldur's Gate 3",
+        type: "video game",
+        genre: "fantasy rpg",
+        numberOfPlayers: "1 to 4",
+        shortDescription: "fantasy adventure role playing game"
+    },
+    {
+        title: "Stardew Valley",
+        type: "video game",
+        genre: "farming sim",
+        numberOfPlayers: "1 to 4",
+        shortDescription: "a pixel art farming simulation game"
+    },
+    {
+        title: "",
+        type: "",
+        genre: "",
+        numberOfPlayers: "",
+        shortDescription: ""
+    },
+    {
+        title: "",
+        type: "",
+        genre: "",
+        numberOfPlayers: "",
+        shortDescription: ""
+    },
 ]
