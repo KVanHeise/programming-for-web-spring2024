807be3ee15c1f2d3ce0b0ce64ab36e70b77b5693
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-01-24 21:12:14 -0600

    started file set-up with objects

---
+++ week2/ex2/script.js
@@ -0,0 +1,13 @@
+// create an array with favorite games as objects inside of the array.
+// title, type, genre, numberOfPlayers, rating, shortDescription
+
+const myFavoriteGames = [
+    {
+        title: "Dead by Daylight",
+        type: "video game",
+        genre: "horror",
+        numberOfPlayers: "up to 5",
+        shortDescription: "co-op horror survival game"
+    },
+
+]
