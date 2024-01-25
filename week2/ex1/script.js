902fb39e83b64ff8bc4f16f6dc759844b91b80f7
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-01-24 20:17:54 -0600

    created array, prompt, alert and console log

---
+++ week2/ex1/script.js
@@ -0,0 +1,11 @@
+let myArr = ["Cabin in the Woods", "Perfume: The Story of a Murderer", "Tucker and Dale VS Evil", "Evil Dead", "The Ring"];
+
+let movie = prompt("What is one of your favorite horror films?");
+
+myArr.push(movie);
+
+alert("Awesome! I'll have to check that one out!");
+
+alert("Some of my favorites in that genre are Cabin in the Woods, Perfume: The Story of a Murderer, Tucker and Dale VS Evil, Evil Dead and The Ring! Check out the console to see our combined list!");
+
+console.log("Our watch list includes:", myArr.toString());
