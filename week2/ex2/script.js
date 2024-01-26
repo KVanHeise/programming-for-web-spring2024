74be7d85159ad318fb76d7d79d8f0b1bbba0e345
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-01-25 18:21:23 -0600

    added object descriptions

--- week2/ex2/script.js
+++ week2/ex2/script.js
@@ -4,37 +4,30 @@
 const myFavoriteGames = [
     {
         title: "Dead by Daylight",
-        type: "video game",
+        developer: "Behavior Interactive Inc.",
         genre: "horror",
         numberOfPlayers: "1 to 5",
-        shortDescription: "co-op horror survival game"
+        synopsis: "Dead by Daylight is a multiplayer (4vs1) horror game where one player takes on the role of the savage Killer, and the other four players play as Survivors, trying to escape the Killer and avoid being caught and killed.",
+    },
+    {
+        title: "Graveyard Keeper",
+        developer: "Lazy Bear Games",
+        genre: "horror / fantasy",
+        numberOfPlayers: "1",
+        synopsis: "Build and manage a medieval graveyard while facing ethical dilemmas and making questionable decisions. Welcome to Graveyard Keeper, the most inaccurate medieval cemetery sim of the year.",
     },
     {
         title: "Baldur's Gate 3",
-        type: "video game",
-        genre: "fantasy rpg",
+        developer: "Larian Studios",
+        genre: "fantasy role playing game",
         numberOfPlayers: "1 to 4",
-        shortDescription: "fantasy adventure role playing game"
+        synopsis: "Baldurs Gate 3 is a story-rich, party-based RPG set in the universe of Dungeons & Dragons, where your choices shape a tale of fellowship and betrayal, survival and sacrifice, and the lure of absolute power.",
     },
     {
         title: "Stardew Valley",
-        type: "video game",
-        genre: "farming sim",
+        developer: "ConcernedApe",
+        genre: "fantasy role playing game / farming simulation",
         numberOfPlayers: "1 to 4",
-        shortDescription: "a pixel art farming simulation game"
-    },
-    {
-        title: "",
-        type: "",
-        genre: "",
-        numberOfPlayers: "",
-        shortDescription: ""
-    },
-    {
-        title: "",
-        type: "",
-        genre: "",
-        numberOfPlayers: "",
-        shortDescription: ""
+        synopsis: "You've inherited your grandfather's old farm plot in Stardew Valley. Armed with hand-me-down tools and a few coins, you set out to begin your new life. Can you learn to live off the land and turn these overgrown fields into a thriving home?",
     },
 ]
