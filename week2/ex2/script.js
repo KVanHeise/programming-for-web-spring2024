054f39985418f3f216ab5b8c6f7b4b142d1a5b83
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-01-25 19:04:21 -0600

    created alert prompts

--- week2/ex2/script.js
+++ week2/ex2/script.js
@@ -5,14 +5,14 @@ const myFavoriteGames = [
     {
         title: "Dead by Daylight",
         developer: "Behavior Interactive Inc.",
-        genre: "horror",
+        genre: "horror game",
         numberOfPlayers: "1 to 5",
         synopsis: "Dead by Daylight is a multiplayer (4vs1) horror game where one player takes on the role of the savage Killer, and the other four players play as Survivors, trying to escape the Killer and avoid being caught and killed.",
     },
     {
         title: "Graveyard Keeper",
         developer: "Lazy Bear Games",
-        genre: "horror / fantasy",
+        genre: "horror / fantasy game",
         numberOfPlayers: "1",
         synopsis: "Build and manage a medieval graveyard while facing ethical dilemmas and making questionable decisions. Welcome to Graveyard Keeper, the most inaccurate medieval cemetery sim of the year.",
     },
@@ -33,3 +33,17 @@ const myFavoriteGames = [
 ]
 
 console.log(myFavoriteGames)
+
+const response = window.prompt("I have four games in my collection. Pick a number between 1 and 4, and I will tell you some information about that game.", "enter a number");
+
+if(response === myFavoriteGames[response -1]); {
+alert("You selected: " + myFavoriteGames[response -1].title + " which is a " + myFavoriteGames[response -1].genre + " developed by " + myFavoriteGames[response -1].developer + ". " + myFavoriteGames[response -1].synopsis)}
+
+if(response === myFavoriteGames[response -2]); {
+alert("You selected: " + myFavoriteGames[response -2].title + " which is a " + myFavoriteGames[response -2].genre + " developed by " + myFavoriteGames[response -2].developer + ". " + myFavoriteGames[response -2].synopsis)}
+
+if(response === myFavoriteGames[response -3]); {
+    alert("You selected: " + myFavoriteGames[response -3].title + " which is a " + myFavoriteGames[response -3].genre + " developed by " + myFavoriteGames[response -3].developer + ". " + myFavoriteGames[response -3].synopsis)}
+
+if(response === myFavoriteGames[response -4]); {
+alert("You selected: " + myFavoriteGames[response -4].title + " which is a " + myFavoriteGames[response -4].genre + " developed by " + myFavoriteGames[response -4].developer + ". " + myFavoriteGames[response -4].synopsis)}
