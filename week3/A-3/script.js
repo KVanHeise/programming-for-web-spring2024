35d4c860524d772bd7617758b3ea29a7c009e79a
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-02-04 20:41:06 -0600

    following along tutorial & added questions to quiz

--- week3/A-3/script.js
+++ week3/A-3/script.js
@@ -11,3 +11,61 @@
 // Optional:
 // Give the user the ability to restart the quiz.
 // Display the number of incorrect answers or the number of incorrect answers until they loose.
+
+let questionInput;
+let currentQuestion;
+let response;
+let responseColor = "green";
+let submitAnswerButton;
+let heading;
+let statements = [
+    {question: "What color do you get when you combine yellow and blue?", answer: "green"},
+    {question: "What color do you get when you combine red and blue?", answer: "purple"},
+    {question: "What color do you get when you combine yellow and red?", answer: "orange"},
+    {question: "When black is added to a color, it is called a:", answer: "shade"},
+    {question: "When white is added to a color, it is called a:", answer: "tint"},
+    {question: "Which color is opposite purple on the color wheel?", answer: "yellow"},
+    {question: "What is the name of a 'true color'? (A color without white or black added to it.)", answer: "hue"}
+];
+
+function next() {
+    if(statements.length < 1) {
+        alert("you won");
+        return;
+    }
+
+    const randomIndex = Math.ceil(Math.random() * statements.length - 1);
+    return statements[randomIndex];
+}
+
+function checkQuestion(){
+    if(currentQuestion.answer === input.value()){
+        //remove correct answer from array
+        statements = statements.filter(statementObj => {
+            return currentQuestion.answer !== statementObj.answer;
+        });
+    }
+}
+
+currentQuestion = next();
+let message = currentQuestion.question;
+
+function setup()
+    {
+    createCanvas(800, 600);
+    background("lightblue");
+    heading = createElement("h2", ["Color Quiz"]);
+    heading.position(100, 100);
+    questionInput = createInput("");
+    questionInput.size(250, 24);
+    questionInput.position(100, 220);
+    submitAnswerButton = createButton("submit answer");
+    submitAnswerButton.size(250, 24);
+    submitAnswerButton.position(100, 260);
+}
+
+function draw() {
+    fill("purple");
+    textSize(24);
+    text(message, 100, 200);
+}
