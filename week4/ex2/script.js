e7705488ba6c2fcdaeedddfc9401e6c3a7e57c41
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-02-09 20:02:10 -0600

    add notes for each section of code

--- week4/ex2/script.js
+++ week4/ex2/script.js
@@ -3,16 +3,22 @@ const rectHeight = 75;
 let rectY;
 const rectWidth = 75;
 let clickCount = 0;
+let speed;
 
 function setup() {
     createCanvas(500, 500);
     rectY = random(height - rectHeight);
+    speed = random(1, 3);
 }
+
+//draw the shapes within the canvas
 
 function draw() {
     background(155);
     drawShape();
-    rectX++;
+    rectX+= speed;
+
+//display score at end of clicking game
 
     if (rectX > width) {
         noLoop();
@@ -20,12 +26,16 @@ function draw() {
     }
 }
 
+//create interactivity for game, track click count
+
 function mousePressed () {
     if ((mouseX >= rectX && mouseX <= rectX + rectWidth) && (mouseY >= rectY && mouseY <= rectY + rectHeight)) {
         clickCount++;
         console.log("hit", clickCount);
     }
 }
+
+//define shape fill
 
 function drawShape() {
     fill("purple");
