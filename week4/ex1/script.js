4baddb51324288143f4c0b9e04f510c62f124b84
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-02-09 19:34:06 -0600

    removed console log and cleaned up notes

--- week4/ex1/script.js
+++ week4/ex1/script.js
@@ -2,9 +2,16 @@
 
 const myEmptyDiv = document.querySelector("#myEmptyDiv");
 
-// put an h1 inside of it
+// put an h1 inside of the div
 // create new element
 
 const heading = document.createElement("h1");
+
 heading.innerHTML = "Hello class! It is a snowy day!";
+heading.style.cursor = "pointer";
+heading.addEventListener("click", handleHeadingClick);
 myEmptyDiv.appendChild(heading);
+
+function handleHeadingClick() {
+    document.body.style.backgroundColor = "blue";
+}
