c5a3bcc4ae3d7b80d70cfc492148ee694e009d92
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-02-11 15:14:00 -0600

    followed example video and increased rows to a 4x4 grid for my card game

--- week4/A-4/script.js
+++ week4/A-4/script.js
@@ -1,16 +1,32 @@
 const circleDiameter = 100;
-let circleX = 200;
-let circleY = 300;
+
+
+let startingX = 200;
+let startingY = 100;
+let myCircles = [];
+let startingId = 0;
 
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(1000, 1000);
     background(0);
-    ellipse(circleX, circleY, circleDiameter);
+    for (let k = 0; k < 4; k++){
+        for (let i = 0; i < 4; i++){
+            ellipse(startingX, startingY, circleDiameter);
+            myCircles.push({x: startingX, y: startingY, id: startingId});
+            startingX += 150;
+            startingId++;
+        }
+        startingY += 150;
+        startingX = 200;
+    }
+    console.log(myCircles)
 }
 
 function mousePressed() {
-    let distance = dist(mouseX, mouseY, circleX, circleY);
-    if (distance < circleDiameter / 2) {
-        console.log("Circle has been clicked");
+    for (let j = 0; j < myCircles.length; j++) {
+        let distance = dist(mouseX, mouseY, myCircles[j].x, myCircles[j].y);
+        if (distance < circleDiameter / 2) {
+            console.log("Circle has been clicked", myCircles[j].id);
+        }
     }
 }
