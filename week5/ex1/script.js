2446c0431578ffb8c3a077e3c43d020ac00ac0fa
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-02-15 15:19:05 -0600

    added exercise instructions

--- week5/ex1/script.js
+++ week5/ex1/script.js
@@ -1,17 +1,7 @@
-// select empty div and assign to a variable
-
-const myEmptyDiv = document.querySelector("#myEmptyDiv");
-
-// put an h1 inside of the div
-// create new element
-
-const heading = document.createElement("h1");
-
-heading.innerHTML = "Hello class! It is a snowy day!";
-heading.style.cursor = "pointer";
-heading.addEventListener("click", handleHeadingClick);
-myEmptyDiv.appendChild(heading);
-
-function handleHeadingClick() {
-    document.body.style.backgroundColor = "blue";
-}
+// Create a p5.js project
+// Create a function that draws a square in a specific location. The fill color and the location should be  function parameters.
+// Instead of using the draw function to draw your block, create a setInterval
+// Each interval, draw your block increase either the X or the Y
+// When the block goes out of bounds, reset whichever axis went out of bounds back to 0 and move the other axis coordinate over the length or width of your block. It should be like starting a new row or column.
+// When the block is beyond both the width and the height, clear your interval.
+// Experiment with keyTyped or keyPressed to change the color of your block as it goes.
