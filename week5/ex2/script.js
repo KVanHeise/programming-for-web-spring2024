d6cdebbef42f5628c41deb330ece22283c11d578
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-02-15 18:27:14 -0600

    created multiple versions of the bubble using for loop in draw function with an array of bubbles

--- week5/ex2/script.js
+++ week5/ex2/script.js
@@ -9,3 +9,41 @@
 // Create a method (a class-specific function)  that renders the shape with the class properties (probably something like this.x andn this.y—you may also want to use class properties with width and height). 
 // Create a method that gives the shape some sort of movement.
 // Use a for loop in the draw function to draw a number of instances of your class on the canvas.
+
+class Bubble {
+    constructor() {
+        this.x = 200;
+        this.y = 150;
+    }
+
+    move() {
+        this.x = this.x + random(-5, 5);
+        this.y = this.y + random(-5, 5);
+    }
+
+    show() {
+        stroke(255);
+        strokeWeight(4);
+        noFill();
+        ellipse(this.x, this.y, 24, 24);
+    }
+}
+
+const bubbles = [
+    new Bubble(),
+    new Bubble(),
+    new Bubble(),
+]
+
+function setup() {
+    createCanvas(600, 400);
+    background(0);
+}
+
+function draw() {
+    background(0);
+    for (let i = 0; i < bubbles.length; i++) {
+        bubbles[i].move();
+        bubbles[i].show();
+    }
+}
