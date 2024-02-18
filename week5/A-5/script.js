763d92197cd0c6ee0e044b06e75dadfc697e2b9a
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-02-18 15:04:28 -0600

    resized and added face images to code

--- week5/A-5/script.js
+++ week5/A-5/script.js
@@ -20,13 +20,29 @@ let cards = [];
 const gameState = {
 
 };
+let cardFaceArray = [];
+
+let cardBack;
+function preload(){
+    cardBack = loadImage("images/card-back.png");
+    cardFaceArray = [
+        loadImage("images/castor.png"),
+        loadImage("images/dani.png"),
+        loadImage("images/dante.png"),
+        loadImage("images/fluke.png"),
+        loadImage("images/ismark.png"),
+        loadImage("images/mirna.png"),
+        loadImage("images/muriel.png"),
+        loadImage("images/urwin.png"),
+    ]
+}
 
 function setup() {
     createCanvas(950, 950);
     background(0);
     for (let j = 0; j < 4; j++) {
         for (let i = 0; i < 4; i++) {
-            cards.push(new Card(startingX, startingY));
+            cards.push(new Card(startingX, startingY, cardFaceArray[0]));
             startingX += 200;
         }
         startingY += 200;
@@ -43,21 +59,24 @@ function mousePressed() {
 }
 
 class Card {
-    constructor (x, y) {
+    constructor (x, y, cardFaceImg) {
         this.x = x;
         this.y = y;
         this.width = 150;
         this.height = 150;
         this.face = DOWN;
+        this.cardFaceImg = cardFaceImg;
         this.show();
     }
     show () {
         if (this.face === DOWN) {
         fill("rgb(57.7%, 9.9%, 9.9%)");
         rect(this.x, this.y, this.width, this.height, 10);
+        image(cardBack, this.x, this.y);
         } else {
             fill("#aaa");
             rect(this.x, this.y, this.width, this.height, 10);
+            image(this.cardFaceImg, this.x, this.y);
         }
     }
 
