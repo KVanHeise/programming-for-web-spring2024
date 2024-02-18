d9aebf6f6f1241dd59e5a62a464b78897b0818e1
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-02-18 14:43:44 -0600

    added card token images & followed along with exercise videos

--- week5/A-5/script.js
+++ week5/A-5/script.js
@@ -9,3 +9,53 @@
 // If the cards don't match, both cards should be returned to face down.
 // Keep score
 // Indicate when the user has completed all the matches
+let myCard;
+const DOWN = "down";
+const UP = "up";
+
+function setup() {
+    createCanvas(800,600);
+    background(0);
+    myCard = new Card();
+}
+
+function mousePressed() {
+    console.log(myCard.didHit(mouseX, mouseY));
+}
+
+class Card {
+    constructor () {
+        this.x = 100;
+        this.y = 100;
+        this.width = 80;
+        this.height = 100;
+        this.face = DOWN;
+        this.show();
+    }
+    show () {
+        if (this.face === DOWN) {
+        fill("rgb(57.7%, 9.9%, 9.9%)");
+        rect(this.x, this.y, this.width, this.height, 10);
+        } else {
+            fill("#aaa");
+            rect(this.x, this.y, this.width, this.height, 10);
+        }
+    }
+
+    didHit (mouseX, mouseY) {
+        if (mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height) {
+            this.flip();
+            return true;
+        } else {
+            return false;
+        }
+    }
+    flip () {
+        if (this.face === DOWN) {
+            this.face = UP;
+        } else {
+            this.face = DOWN;
+        }
+        this.show();
+    }
+}
