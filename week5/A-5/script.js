6c7d5935872a87adf4141a4e6444cb0dc9fa99a8
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-02-18 11:22:46 -0600

    added assignment instructions as notes

---
+++ week5/A-5/script.js
@@ -0,0 +1,11 @@
+// Assignment Description:
+
+// Complete your memory game. Incorporate your artwork and provide code for the interactivity. The final project should:
+
+// Display an even collection of 'cards' face down.
+// Clicking on a card should display the 'face' of the card.
+// Only two cards should be face up at a time.
+// If the two card faces match, they should remain up.
+// If the cards don't match, both cards should be returned to face down.
+// Keep score
+// Indicate when the user has completed all the matches
