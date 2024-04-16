2a619c990dbbb37862f8a310a800cf87109e2dbb
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-04-16 16:33:16 -0500

    set up scaffolding for llama facts and showing them on the page

--- week6/A-6/llama-facts/src/App.jsx
+++ week6/A-6/llama-facts/src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+
+  const llamaFacts = [
+    "llama fact 1",
+    "llama fact 2",
+    "llama fact 3",
+    "llama fact 4",
+  ];
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Llama Facts</h1>
+        {llamaFacts}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
