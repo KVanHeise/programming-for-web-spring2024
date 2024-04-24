fb949aa5372f31956460dfff6c01020b2c736232
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-04-24 17:26:26 -0500

    finished css demo; added files for rendering lists demo

--- week7/ex2/css-demo/src/App.jsx
+++ week7/ex2/css-demo/src/App.jsx
@@ -2,8 +2,18 @@ import "./styles.css"
 
 function App() {
   const isKenOld = "yes";
+  const paragraphStyleObj = {
+    color: "green",
+    fontSize: "36px",
+    margin: "20px auto",
+    width: "600px"
+  }
+
   return (
-    <h1 className={isKenOld === "yes" ? "blue" : "red"}>CSS Demo</h1>
+    <>
+      <h1 className={isKenOld === "yes" ? "blue" : "red"}>CSS Demo</h1>
+      <p style={paragraphStyleObj}>This is a random test</p>
+    </>
   )
 }
 
