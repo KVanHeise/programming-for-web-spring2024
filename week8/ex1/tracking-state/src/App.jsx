06d947ab7a8599e3b2bd0ac7febc379dc6490e09
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-04-25 01:17:33 -0500

    finished setting up files using walkthru of state

--- week8/ex1/tracking-state/src/App.jsx
+++ week8/ex1/tracking-state/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [leftScore, setLeftScore] = useState(0)
+  const [rightScore, setRightScore] = useState(0)
 
   return (
     <>
+      <h1>Solitare Clicky Game</h1>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        left
+        <button onClick={() => {
+        setLeftScore(leftScore +1);
+        }}> Push Me!
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div>
+        right
+        <button onClick={() => {
+        setRightScore(rightScore +1);
+        }}>No, Push Me!
+        </button>
+      </div>
+
+      <div>
+        <h2>Scores</h2>
+        Left Score {leftScore} | Right Score {rightScore}
+      </div>
     </>
   )
 }
