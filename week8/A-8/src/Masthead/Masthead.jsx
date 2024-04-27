6fb1ff93a4e9b577287ff6fe72a9345e18a92ccd
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-04-27 13:15:00 -0500

    trying to fix why masthead isn't linking

--- week8/A-8/src/Masthead/Masthead.jsx
+++ week8/A-8/src/Masthead/Masthead.jsx
@@ -3,7 +3,7 @@ import mastheadImg from "../assets/masthead.png";
 export default function Masthead() {
     return (
         <div className="masthead">
-            <img src={mastheadImg} alt="Curse of Strahd Roster" />
+            <img src={mastheadImg} alt="Curse of Strahd Roster Masthead" />
             <h1>Active Character Roster</h1>
         </div>
     )
