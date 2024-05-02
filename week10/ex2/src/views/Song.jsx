b5a8e3860c1a6afe644c1c6cd7ae0ce85dcd462a
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-05-02 03:07:15 -0500

    completed files, working along with demo video

---
+++ week10/ex2/src/views/Song.jsx
@@ -0,0 +1,24 @@
+import { Link, useParams } from "react-router-dom";
+import PropTypes from "prop-types";
+
+export function Song({data}) {
+    const {slug} = useParams();
+    const selectedSong = data.find((song) => song.slug === slug)
+
+    return (
+        <>
+        <nav>
+        <Link to="/">Go Back Home</Link>
+        </nav>
+
+        <h1>{selectedSong.title}</h1>
+        <img src={selectedSong.cover} alt={selectedSong.title} />
+        <p>Artist: {selectedSong.artist}</p>
+        <p>Rank: {selectedSong.rank}</p>
+        </>
+    )
+}
+
+Song.propTypes = {
+    data: PropTypes.arrayOf(PropTypes.any)
+}
