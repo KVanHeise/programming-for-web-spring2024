a03d63b82a06184f7342d5cc431d8a33819cb2b2
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-05-02 04:08:48 -0500

    added CSS for deckdetails

--- week10/A-10/src/views/DeckDetail.jsx
+++ week10/A-10/src/views/DeckDetail.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
-import {useParams} from "react-router-dom";
+import {useParams, Link} from "react-router-dom";
+import "./DeckDetails.css";
 
 export function DeckDetail({data}) {
     const { id } = useParams();
@@ -7,14 +8,20 @@ export function DeckDetail({data}) {
     const selectedDeck = data.find((deck) => deck.id === id);
     console.log
     return(
-        <>
+        <div className="page">
+        <Link to="/">Return to Home</Link>
+
         <h1>{selectedDeck.charName}</h1>
+
         <img src={selectedDeck.image} alt={selectedDeck.charName} />
+
         <p>Species: {selectedDeck.species}</p>
+
         <p>Class {selectedDeck.powerset}</p>
+
         <p>Background: {selectedDeck.backstory}</p>
 
-        </>
+        </div>
     )
 }
 
