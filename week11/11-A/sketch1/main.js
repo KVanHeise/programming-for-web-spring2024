d7f65ab2977f80ed81ff9a88626c07ccb0fe9aa5
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-05-03 14:51:55 -0500

    finished functions in js & added CSS

--- week11/11-A/sketch1/main.js
+++ week11/11-A/sketch1/main.js
@@ -1,36 +1,44 @@
-// Click and drag the mouse to view the scene from different angles.
+//Use the arrow keys on your keyboard to explore a 3D sphere/panorama image.
 
+let x = -30, y = -30;
 let img;
 
 // Load an image and create a p5.Image object.
 function preload() {
-  img = loadImage("/assets/japanese-garden.jpg");
+  img = loadImage("assets/san-juans.jpg");
 }
+
+//photo taken from vacation to San Juan islands on the Puget Sound!
 
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(1000, 1000, WEBGL);
 }
 
+  // Add the panorama on the canvas
 function draw() {
-  createImage(img);
+  panorama();
 }
 
-// function draw() {
-//   // Add the panorama.
-//   panorama(img);
+  // define the panorama behavior and view
+function panorama() {
+  noStroke();
+  camera();
+  scale(-1, 1, 1)
+  rotateX(radians(y));
+  rotateY(radians(x));
 
-//   // Enable orbiting with the mouse.
-//   orbitControl();
+  //set background image for sphere and sphere size
 
-//   // Use the image as a light source.
-//   imageLight(img);
+  texture(img);
+  sphere(5000);
 
-//   // Style the sphere.
-//   noStroke();
-//   specularMaterial(50);
-//   shininess(200);
-//   metalness(100);
-
-//   Draw the sphere.
-//   sphere(30);
-// }
+  // allow panorama rotation with arrow keys
+  // honestly googled how to move around in a panorama and discovered p5 sketches: https://editor.p5js.org/ElenaPeng/sketches/1QBjWElFD
+  // I changed up the variables and adjusted the starting view position, but utilized their methods to learn.
+  // used this site recommended on p5 for keycodes: https://www.toptal.com/developers/keycode
+  
+  if (keyIsDown(39)) x--;
+  if (keyIsDown(37)) x++;
+  if (keyIsDown(38) && y > -90) y--;
+  if (keyIsDown(40) && y < 90) y++;
+}
