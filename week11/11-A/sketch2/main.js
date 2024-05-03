e977e2e4ecce814778524abf700281cf02fd912a
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-05-03 16:01:13 -0500

    created a spinning box in the void!

--- week11/11-A/sketch2/main.js
+++ week11/11-A/sketch2/main.js
@@ -1,44 +1,30 @@
-//Use the arrow keys on your keyboard to explore a 3D sphere/panorama image.
+//Create a "photo cube" that can be rotated by the viewer using their mouse.
 
-let x = -30, y = -30;
 let img;
 
-// Load an image and create a p5.Image object.
 function preload() {
   img = loadImage("assets/san-juans.jpg");
 }
-
-//photo taken from vacation to San Juan islands on the Puget Sound!
 
 function setup() {
   createCanvas(1000, 1000, WEBGL);
 }
 
-  // Add the panorama on the canvas
 function draw() {
-  panorama();
-}
+  background(000);
 
-  // define the panorama behavior and view
-function panorama() {
-  noStroke();
-  camera();
-  scale(-1, 1, 1)
-  rotateX(radians(y));
-  rotateY(radians(x));
+  //allows mouse to rotate box
+  orbitControl();
 
-  //set background image for sphere and sphere size
+  //allows box to rotate on its own in a loop
+
+  rotateZ(frameCount * 0.005);
+  rotateX(frameCount * 0.005);
+  rotateY(frameCount * 0.005);
 
   texture(img);
-  sphere(5000);
 
-  // allow panorama rotation with arrow keys
-  // honestly googled how to move around in a panorama and discovered p5 sketches: https://editor.p5js.org/ElenaPeng/sketches/1QBjWElFD
-  // I changed up the variables and adjusted the starting view position, but utilized their methods to learn.
-  // used this site recommended on p5 for keycodes: https://www.toptal.com/developers/keycode
-  
-  if (keyIsDown(39)) x--;
-  if (keyIsDown(37)) x++;
-  if (keyIsDown(38) && y > -90) y--;
-  if (keyIsDown(40) && y < 90) y++;
+
+  box(330);
+
 }
