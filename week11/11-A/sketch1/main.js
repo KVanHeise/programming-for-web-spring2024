601e92bbb433ad45dfdef255bc2b2c5d64b79b02
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-05-03 13:25:12 -0500

    removed code to just display canvas to create project from scratch using p5 instructions

--- week11/11-A/sketch1/main.js
+++ week11/11-A/sketch1/main.js
@@ -1,39 +1,37 @@
 // Click and drag the mouse to view the scene from different angles.
 
 let img;
-let describe;
-let panorama;
-let imageLight;
-let shininess;
-let metalness;
 
 // Load an image and create a p5.Image object.
 function preload() {
-  img = loadImage('assets/japanese-garden.jpg');
+  img = loadImage('./assets/japanese-garden.jpg');
 }
 
 function setup() {
-  createCanvas(100 ,100 ,WEBGL);
-
-  describe('A sphere floating above a landscape. The surface of the sphere reflects the landscape. The full landscape is viewable in 3D as the user drags the mouse.');
+  createCanvas(1000, 1000, WEBGL);
+  background(51);
 }
 
 function draw() {
-  // Add the panorama.
-  panorama(img);
 
-  // Enable orbiting with the mouse.
-  orbitControl();
-
-  // Use the image as a light source.
-  imageLight(img);
-
-  // Style the sphere.
-  noStroke();
-  specularMaterial(50);
-  shininess(200);
-  metalness(100);
-
-  // Draw the sphere.
-  sphere(30);
 }
+
+// function draw() {
+//   // Add the panorama.
+//   panorama(img);
+
+//   // Enable orbiting with the mouse.
+//   orbitControl();
+
+//   // Use the image as a light source.
+//   imageLight(img);
+
+//   // Style the sphere.
+//   noStroke();
+//   specularMaterial(50);
+//   shininess(200);
+//   metalness(100);
+
+//   Draw the sphere.
+//   sphere(30);
+// }
