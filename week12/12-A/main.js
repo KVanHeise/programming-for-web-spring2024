aa1605c58a4ba22aaef262b70bdc55e6d49678d9
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-05-03 23:15:18 -0500

    following along with video tutorial to set up soundboard

--- week12/12-A/main.js
+++ week12/12-A/main.js
@@ -1,1 +1,30 @@
 // create a digital instrument!
+
+function setup() {
+  createCanvas (windowWidth, windowHeight);
+  background("black");
+
+  let pad1 = new Pad(10, 10);
+  pad1.draw();
+}
+
+class Pad {
+  constructor(x, y) {
+    // this is the actual music pad
+
+    this.x = x;
+    this.y = y;
+
+    //Oscillator, Envelope, Amplitude objects.
+
+    this.osc = new p5.Oscillator;
+
+  }
+
+  draw() {
+  
+    fill("orange");
+    rect(this.x, this.y, 150, 150);
+
+  }
+}
