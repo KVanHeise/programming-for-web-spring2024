042f70d5260641f0a6ad39417c489c785a4deded
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-05-04 01:36:39 -0500

    updated; finally got sound working!

--- week12/ex2/main.js
+++ week12/ex2/main.js
@@ -1,63 +1,48 @@
-// create a digital instrument!
-let pad1;
+let song;
+let button;
+// var button;
+// var amp;
+
+// function setup() {
+//   createCanvas(200, 200);
+//   song = loadSound("./sounds/Cinematic-Atmospheric-Intro.wav", loaded)
+//   amp = new p5.Amplitude();
+// }
+
+// function loaded() {
+//   button = createButton("play");
+//   button.mousePressed(togglePlaying);
+// }
+
+// function draw() {
+//   background(51);
+
+//   var vol = amp.getLevel();
+
+//   ellipse(width / 2, height / 2, vol, vol)
+// }
 
 function setup() {
-  createCanvas (windowWidth, windowHeight);
-  background("black");
+  createCanvas(200, 200);
+  song = loadSound("./sounds/Cinematic-Atmospheric-Intro.wav", loaded);
+  background(51)
+}
 
-  pad1 = new Pad(10, 10);
+function loaded() {
+  button = createButton("play");
+  button.mousePressed(togglePlaying);
+}
+
+function togglePlaying() {
+  if (!song.isPlaying()) {
+    song.play();
+    button.html("pause");
+  } else {
+    song.stop();
+    button.html("play");
+  }
 }
 
 function draw() {
-  pad1.draw();
-}
-
-function mouseClicked() {
-  pad1.play();
-}
-
-class Pad {
-  constructor(x, y) {
-    // this is the actual music pad
-
-    this.x = x;
-    this.y = y;
-
-    //Oscillator, Envelope, Amplitude objects.
-    this.osc = new p5.Oscillator();
-    this.osc.amp(0);
-    this.osc.setType("sine");
-    this.osc.start();
-
-    //Envelope
-
-    this.env = new p5.Envelope();
-    this.env.setADSR(0.001, .1, .2, .1);
-    this.env.setRange(3, 0);
-
-    //Amplitude
-
-    this.analyzer = new p5.Amplitude();
-    this.analyzer.setInput(this.env);
-
-  }
-
-  draw() {
-    let level = this.analyzer.getLevel();
-    let levelHeight = map(level, 0, 2.5, 0, 150);
-  
-    fill("orange");
-    rect(this.x, this.y, 150, 150);
-
-
-    fill("white");
-    rect(this.x, this.y, 150, levelHeight);
-
-  }
-
-  play() {
-    this.osc.start();
-    this.osc.freq(130);
-    this.env.play(this.osc);
-  }
+  background(0);
 }
