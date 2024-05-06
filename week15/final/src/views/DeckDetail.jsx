568313fbcc6b9e4fafe12ed4d8fd665c0515657f
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2024-05-06 17:19:29 -0500

    cleaned up spacing

--- week15/final/src/views/DeckDetail.jsx
+++ week15/final/src/views/DeckDetail.jsx
@@ -9,30 +9,30 @@ export function DeckDetail({data}) {
     console.log
     return(
         <div className="project-info-page">
-        <nav>
-            <ul>
-            <li><Link to="/" className="home">Home</Link></li>
-            <li><Link to="/About" className="about">About</Link></li>
-            <li><Link to="/Contact" className="about">Contact</Link></li>
-            </ul>
-        </nav>
+            <nav>
+                <ul>
+                <li><Link to="/" className="home">Home</Link></li>
+                <li><Link to="/About" className="about">About</Link></li>
+                <li><Link to="/Contact" className="about">Contact</Link></li>
+                </ul>
+            </nav>
 
-        <h1>{selectedDeck.projectName}</h1>
+            <h1>{selectedDeck.projectName}</h1>
 
-        <img src={selectedDeck.image} alt={selectedDeck.projectName} className="fullprofile-img" />
+            <img src={selectedDeck.image} alt={selectedDeck.projectName} className="fullprofile-img" />
 
-        <img src={selectedDeck.image2} alt={selectedDeck.projectName} className="fullprofile-img" />
+            <img src={selectedDeck.image2} alt={selectedDeck.projectName} className="fullprofile-img" />
 
-        <div className="project-details">
+            <div className="project-details">
 
             <p>{selectedDeck.projectType}</p>
 
             </div>
 
-        <footer>
-        <hr />
-        <p>Kyrin Van Heise  |  Programming for Web  |  Spring 2024</p>
-        </footer>
+            <footer>
+                <hr />
+                <p>Kyrin Van Heise  |  Programming for Web  |  Spring 2024</p>
+            </footer>
         </div>
     )
 }
